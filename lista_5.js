f7177c0cfb24d2c8728559cd10e551be32b00dd1
Kauan-Azevedo <kauanazevedo2010@gmail.com>
2024-08-20 21:27:53 -0300

    feat: lista 5

---
+++ lista_5.js
@@ -0,0 +1,18 @@
+const atividade1 = () => {
+  let array = [];
+  array[1] = "ana";
+  array[3] = "claudio";
+  array[7] = "jose";
+
+  console.log(array.length);
+  console.log(array);
+};
+
+const atividade2 = () => {
+  let array = [1, 2, 3, 4, 5];
+  console.log(array);
+  array[0] = 100;
+  console.log(array);
+};
+
+atividade2()
